a986b3f81f36e9ba2c580a4434cdc9f8472a1b02
NISHU-PC-MSIGT7\User <gupta.nisha@husky.neu.edu>
2017-05-04 17:56:17 -0400

    ---

--- Financial Literacy Game/Assets/Scripts/DropdownController.cs
+++ Financial Literacy Game/Assets/Scripts/DropdownController.cs
@@ -7,13 +7,38 @@ public class DropdownController : MonoBehaviour {
 
     List<string> names = new List<string>() { "Choose a Goal", "Car", "College", "House" };
     public Dropdown dropdown;
-    public Text selectedName;
+    public string selectedName;
+    public Text goalAmountText;
+    
 
     public void Dropdown_IndexChanged(int index)
     {
-        selectedName.text = names[index];
+        selectedName = names[index];
+        PlayerPrefs.SetString("Goal", selectedName);
+        goalAmountText.text = "The goal amount text is:  " + GetGoalAmount();
+        Debug.Log("The " + "goal " + " is " + PlayerPrefs.GetString("Goal"));
     }
 
+    public int GetGoalAmount()
+    {
+        if (selectedName.Equals("Car"))
+        {
+            return 2000;
+        }
+
+        if (selectedName.Equals("College"))
+        {
+            return 16000;
+        }
+
+        if (selectedName.Equals("House"))
+        {
+            return 30000;
+        }
+
+        else return 0;
+
+    }
 	
 	void Start () {
 
