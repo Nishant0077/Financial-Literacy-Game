061e2c9fdd3654b233cc14ba88f4f1c525e95ad8
NISHU-PC-MSIGT7\User <gupta.nisha@husky.neu.edu>
2017-05-04 17:15:33 -0400

    Playing around with the first scenario - added a dropdown for selecting goal

---
+++ Financial Literacy Game/Assets/Scripts/DropdownController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownController : MonoBehaviour {
+
+    List<string> names = new List<string>() { "Choose a Goal", "Car", "College", "House" };
+    public Dropdown dropdown;
+    public Text selectedName;
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        selectedName.text = names[index];
+    }
+
+	
+	void Start () {
+
+        PopulateList();
+		
+	}
+	
+	void PopulateList()
+    {
+        dropdown.AddOptions(names);
+    }
+}
